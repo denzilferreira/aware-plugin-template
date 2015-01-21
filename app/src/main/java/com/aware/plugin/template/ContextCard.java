8676b35fc329034b5271934dbcde851fdbbadae8
denzilferreira <denzil.ferreira@ee.oulu.fi>
2015-01-21 22:54:37 +0200

    More complete template.

--- app/src/main/java/com/aware/plugin/template/ContextCard.java
+++ app/src/main/java/com/aware/plugin/template/ContextCard.java
@@ -1,6 +1,7 @@
 package com.aware.plugin.template;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.aware.utils.IContextCard;
@@ -13,7 +14,11 @@ public class ContextCard implements IContextCard {
     @Override
     public View getContextCard(Context context) {
         //Inflate and return your card's layout. See LayoutInflater documentation.
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View card = (View) inflater.inflate(R.layout.card, null);
 
-        return null;
+        //Modify card's content
+
+        return card;
     }
 }
