9668d2a0164c5b5ee7c0ee1bd81a8f40bb8310b8
denzilferreira <ferreiradenzil@gmail.com>
2016-05-04 15:54:02 +0300

    - Updated for 4.0 series.

--- app/src/main/java/com/aware/plugin/template/Plugin.java
+++ app/src/main/java/com/aware/plugin/template/Plugin.java
@@ -2,10 +2,13 @@ package com.aware.plugin.template;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
+import com.aware.ui.PermissionsHandler;
 import com.aware.utils.Aware_Plugin;
 
 public class Plugin extends Aware_Plugin {
@@ -36,6 +39,11 @@ public class Plugin extends Aware_Plugin {
         //Add permissions you need (Support for Android M) e.g.,
         REQUIRED_PERMISSIONS.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+        Intent permissions = new Intent(this, PermissionsHandler.class);
+        permissions.putExtra(PermissionsHandler.EXTRA_REQUIRED_PERMISSIONS, REQUIRED_PERMISSIONS);
+        permissions.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(permissions);
+
         //To sync data to the server, you'll need to set this variables from your ContentProvider
         DATABASE_TABLES = Provider.DATABASE_TABLES;
         TABLES_FIELDS = Provider.TABLES_FIELDS;
@@ -48,9 +56,11 @@ public class Plugin extends Aware_Plugin {
     //This function gets called every 5 minutes by AWARE to make sure this plugin is still running.
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        //Check if the user has toggled the debug messages
-        DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
-
+        //Check if we still have the permissions/are we good to go?
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            //Check if the user has toggled the debug messages
+            DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
