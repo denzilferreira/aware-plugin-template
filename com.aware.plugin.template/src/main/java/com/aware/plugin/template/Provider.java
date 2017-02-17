490b8b40ce55dd1eae75b3b53327c5f1da7b0ef7
Denzil Ferreira <denzil.ferreira@ee.oulu.fi>
2017-02-17 13:09:26 +0200

    - Fix database leaks.

--- com.aware.plugin.template/src/main/java/com/aware/plugin/template/Provider.java
+++ com.aware.plugin.template/src/main/java/com/aware/plugin/template/Provider.java
@@ -78,21 +78,18 @@ public class Provider extends ContentProvider {
 
     //Helper variables for ContentProvider - DO NOT CHANGE
     private UriMatcher sUriMatcher;
-    private DatabaseHelper databaseHelper;
+    private static DatabaseHelper dbHelper;
+    private static SQLiteDatabase database;
+    private void initialiseDatabase() {
+        if (dbHelper == null)
+            dbHelper = new DatabaseHelper(getContext(), DATABASE_NAME, null, DATABASE_VERSION, DATABASE_TABLES, TABLES_FIELDS);
+        if (database == null)
+            database = dbHelper.getWritableDatabase();
+    }
     //--
 
     //For each table, create a hashmap needed for database queries
     private HashMap<String, String> tableOneHash;
-
-    /**
-     * Initialise database: create the database file, update if needed, etc. DO NOT CHANGE ME
-     * @return
-     */
-    private void initializeDB() {
-        if (databaseHelper == null) {
-            databaseHelper = new DatabaseHelper(getContext(), DATABASE_NAME, null, DATABASE_VERSION, DATABASE_TABLES, TABLES_FIELDS);
-        }
-    }
 
     @Override
     public boolean onCreate() {
@@ -114,15 +111,14 @@ public class Provider extends ContentProvider {
         tableOneHash.put(TableOne_Data.BIG_NUMBER, TableOne_Data.BIG_NUMBER);
         tableOneHash.put(TableOne_Data.PICTURE, TableOne_Data.PICTURE);
 
+        initialiseDatabase();
+
         return true;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        initializeDB();
-
-        SQLiteDatabase database = databaseHelper.getWritableDatabase();
-        if (database == null) return 0;
+        initialiseDatabase();
 
         database.beginTransaction();
 
@@ -149,10 +145,8 @@ public class Provider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues initialValues) {
-        initializeDB();
 
-        SQLiteDatabase database = databaseHelper.getWritableDatabase();
-        if (database == null) return null;
+        initialiseDatabase();
 
         ContentValues values = (initialValues != null) ? new ContentValues(initialValues) : new ContentValues();
 
@@ -182,10 +176,7 @@ public class Provider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
-        initializeDB();
-
-        SQLiteDatabase database = databaseHelper.getReadableDatabase();
-        if (database == null) return null;
+        initialiseDatabase();
 
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
         switch (sUriMatcher.match(uri)) {
@@ -232,10 +223,7 @@ public class Provider extends ContentProvider {
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
 
-        initializeDB();
-
-        SQLiteDatabase database = databaseHelper.getWritableDatabase();
-        if (database == null) return 0;
+        initialiseDatabase();
 
         database.beginTransaction();
 
