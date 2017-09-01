eacd837a3b6181c37239a3ca923f0715a0ffbfde
Denzil Ferreira <denzil.ferreira@ee.oulu.fi>
2017-09-01 11:38:33 +0300

    - Updated to use sync-adapters instead of the

---
+++ com.aware.plugin.template/src/main/java/com/aware/plugin/template/syncadapters/Template_Sync.java
@@ -0,0 +1,42 @@
+package com.aware.plugin.template.syncadapters;
+
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.aware.plugin.template.Provider;
+import com.aware.syncadapters.AwareSyncAdapter;
+
+/**
+ * Created by denzilferreira on 01/09/2017.
+ *
+ * This class tells what data is synched to the server. The Uri[] needs to be in the same order as the database tables and tables fields (due to the index in the array).
+ */
+public class Template_Sync extends Service {
+    private AwareSyncAdapter sSyncAdapter = null;
+    private static final Object sSyncAdapterLock = new Object();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        synchronized (sSyncAdapterLock) {
+            if (sSyncAdapter == null) {
+                sSyncAdapter = new AwareSyncAdapter(getApplicationContext(), true, true);
+                sSyncAdapter.init(
+                        Provider.DATABASE_TABLES, Provider.TABLES_FIELDS,
+                        new Uri[]{
+                                Provider.TableOne_Data.CONTENT_URI
+                        }
+                );
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSyncAdapter.getSyncAdapterBinder();
+    }
+}
