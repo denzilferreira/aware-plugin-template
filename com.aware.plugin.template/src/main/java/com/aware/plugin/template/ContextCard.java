47e49c8b676d403313cb1f8cb78456297f485ad2
Denzil Ferreira <ferreiradenzil@gmail.com>
2017-09-02 22:30:51 +0300

    - Added example of real-time sensor data callbacks functionality

--- com.aware.plugin.template/src/main/java/com/aware/plugin/template/ContextCard.java
+++ com.aware.plugin.template/src/main/java/com/aware/plugin/template/ContextCard.java
@@ -1,26 +1,47 @@
 package com.aware.plugin.template;
 
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.aware.utils.IContextCard;
 
 public class ContextCard implements IContextCard {
 
     //Constructor used to instantiate this card
-    public ContextCard() {}
+    public ContextCard() {
+    }
+
+    private TextView hello = null;
 
     @Override
     public View getContextCard(Context context) {
         //Load card layout
         View card = LayoutInflater.from(context).inflate(R.layout.card, null);
+        hello = card.findViewById(R.id.hello);
 
-        //Initialize UI elements from the card
-
-        //Set data on the UI
+        //Register the broadcast receiver that will update the UI from the background service (Plugin)
+        IntentFilter filter = new IntentFilter("ACCELEROMETER_DATA");
+        context.registerReceiver(accelerometerObserver, filter);
 
         //Return the card to AWARE/apps
         return card;
+    }
+
+    //This broadcast receiver is auto-unregistered because it's not static.
+    private AccelerometerObserver accelerometerObserver = new AccelerometerObserver();
+    public class AccelerometerObserver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equalsIgnoreCase("ACCELEROMETER_DATA")) {
+                ContentValues data = intent.getParcelableExtra("data");
+                hello.setText(data.toString());
+            }
+        }
     }
 }
